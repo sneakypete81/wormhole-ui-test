0c8e3e12a936644d3f7c1f973110acea17a0dde3
sneakypete81 <sneakypete81@gmail.com>
2020-04-26 14:25:37 +0100

    Prevent race by waiting for entire window plus receive message

--- test_rx.py
+++ test_rx.py
@@ -9,9 +9,8 @@ keyboard = Keyboard()
 connect_dialog.wait_until_visible()
 keyboard.type("981891-wormholeui-test")
 connect_dialog.set_code_button.click()
-main_window.blank.wait_until_visible()
 
-main_window.rx_receive_message.wait_until_visible()
+main_window.window_with_rx_receive_message.wait_until_visible()
 
 keyboard.type("Hello from receive test")
 main_window.send_message_button.click()
